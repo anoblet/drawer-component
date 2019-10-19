432057ffe0cf6bcc9b64f0f9ffda594b1fa14f64
Andrew Noblet <andrewbnoblet@gmail.com>
2019-10-19 10:02:58 -0400

    Chore: Save

--- src/template.ts
+++ src/template.ts
@@ -2,7 +2,7 @@ import { html } from "lit-element";
 
 export default function() {
   return html`
-    <aside part="aside"><slot name="drawer"></slot></aside>
+    <aside part="aside"><slot name="aside"></slot></aside>
     <main part="main"><slot name="main"></slot></main>
   `;
 }
